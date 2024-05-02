b2b361463f60b6d5fc1ba8210d9e83f06db7f8d0
Ferreira <ferreirah.gabriell@gmail.com>
2024-05-02 15:20:15 -0300

    No comments

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -2,10 +2,9 @@ import java.util.Scanner;
 
 public class ContaTerminal { 
     public static void main(String[] args) {
-        //TODO: Importar a classe Scanner
+    
         Scanner scanner = new Scanner(System.in);
 
-        //Exibir as mensagens para o nosso usuario
         System.out.println("Digite sua agencia: ");
         String agencia = scanner.nextLine();
 
@@ -20,10 +19,7 @@ public class ContaTerminal {
         System.out.println("Qual o saldo em sua conta?: ");
         double saldo = scanner.nextDouble();
 
-        //Exibir a mensagem conta criada
         System.out.println("Ola " + nome +", obrigado por criar uma conta em nosso banco, sua agencia e " + agencia + ", conta "+ numero + " e seu saldo "+ saldo + " ja esta disponivel para saque.");
-
-
 
 
         scanner.close();
